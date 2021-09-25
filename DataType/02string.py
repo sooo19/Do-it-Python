8dd205e9776b6187cafcad54f8e1735244a919f3
sooo19 <smile_cindy@naver.com>
2021-09-25 20:49:20 +0900

    문자열 자료형

---
+++ DataType/02string.py
@@ -0,0 +1,14 @@
+#문자열 연산하기
+length=len("You need python")
+print("문장의 길이: "+str(length))
+
+#문자열 포매팅
+#1) 숫자 바로 대입하기
+print("I read %d books." %6)
+
+#2) 문자열 바로 대입하기
+print("I read %s books." %"six")
+
+#3) 숫자 값을 나타내는 변수로 대입하기
+num=6
+print("I read %d books." %num)
